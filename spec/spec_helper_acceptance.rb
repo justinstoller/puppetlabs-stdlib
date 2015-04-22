9d17763782b3e698f352702559751512ce95cb7f
Justin Stoller <justin.stoller@gmail.com>
2015-04-22 09:25:17 -0700

    examples of using AIO builds

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,24 +2,46 @@
 require 'beaker-rspec'
 
 UNSUPPORTED_PLATFORMS = []
+NIGHTLY_MSI = "http://nightlies.puppetlabs.com/puppet-agent-latest/repos/windows/puppet-agent-%s.msi"
 
 unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
-  foss_opts = {
-    :default_action => 'gem_install',
-    :version        => (ENV['PUPPET_VERSION'] ? ENV['PUPPET_VERSION'] : '3.7.2'),
-  }
 
-  if default.is_pe?; then install_pe; else install_puppet( foss_opts ); end
+  if default.is_pe?
+    install_pe
+  else
+    if options[:type] == 'aio' and ENV['DEVEL_PUPPET']
+      hosts.each do |host|
+
+        if host['platform'] =~ /windows/
+          arch = host.is_x86_64? ? 'x64' : 'x86'
+          install_puppet_from_msi(host, :url => NIGHTLY_MSI % arch)
+        else
+          install_puppetlabs_dev_repo(host, 'puppet-agent', ENV['PUPPET_AGENT_VERSION'] || '1.0.0')
+          install_package(host, 'puppet-agent')
+        end
+
+        if host['roles'].include?('master')
+          install_puppetlabs_dev_repo(host, 'puppetserver', ENV['PUPPET_SERVER_VERSION'] || '2.0.0')
+          install_package(host, 'puppetserver')
+        end
+      end
+
+    elsif options[:type] == 'aio'
+      install_puppet(:version              => ENV['PUPPET_VERSION'] || '4.0.0',
+                     :puppet_agent_version => ENV['PUPPET_AGENT_VERSION'] || '1.0.0',
+                     :default_action       => 'gem_install'                             )
+
+    else
+      install_puppet(:version        => ENV['PUPPET_VERSION'] || '3.7.2',
+                     :default_action => 'gem_install'                     )
+    end
+  end
 
   hosts.each do |host|
     if host['platform'] !~ /windows/i
-      if host.is_pe?
-        on host, 'mkdir -p /etc/puppetlabs/facter/facts.d'
-      else
-        on host, "/bin/touch #{host['puppetpath']}/hiera.yaml"
-        on host, "mkdir -p #{host['distmoduledir']}"
-        on host, 'mkdir -p /etc/facter/facts.d'
-      end
+      on host, "/bin/touch #{host['puppetpath']}/hiera.yaml"
+      on host, "mkdir -p #{host['distmoduledir']}"
+      on host, "mkdir -p #{host.puppet['pluginfactdest']}"
     end
   end
 end
